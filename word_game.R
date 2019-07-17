eae7ac1a1de9118c0b74182665038219430a44dd
liam-crow <liam.m.crowhurst@gmail.com>
2019-07-17 10:58:04 +0000

    Example usage

--- word_game.R
+++ word_game.R
@@ -1,9 +1,9 @@
-words <- read.table('words_alpha.txt',header = T,stringsAsFactors = F)
-
 library(stringr)
 library(gtools)
 library(dplyr)
 library(tidyr)
+
+words <- read.table('words_alpha.txt', header = T, stringsAsFactors = F)
 
 word_game_function <- function(input, min){
   x <- unlist(strsplit(input, split = "")) # Splits the string into single characters
@@ -11,14 +11,15 @@ word_game_function <- function(input, min){
   merge <- NULL # initialise
   
   for(i in min:l) {
-    if(l>7){print('Word is too long!'); break}
     # The game has a maximum of 7 characters at a time, also stops long words
     # from taking up computation time
+    if(l>7){print('Word is too long!'); break}
     perm <- as.data.frame(permutations(n=l, r=i, v=x, set=F)) # find every permutation at each length
     merge <- rbind(merge, unique(unite(perm, a, everything(), sep = ""))) # Store all permutations
   }
   return(intersect(words, merge)) # find words that exist in permutations and dictionary, return result
 }
 
-word_game_function('dveeni', 4)
+# Example usage
+word_game_function('pollops', 4)
 
